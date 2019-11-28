56902d8e8d63c87ec798bad212ee358b08a793dc
messer82 <podinamarius82@gmail.com>
2019-11-28 18:55:18 +0200

    Initial commit

---
+++ src/main/java/Intro_to_java/CheckPalindrom.java
@@ -0,0 +1,23 @@
+package Intro_to_java;
+
+import java.util.Scanner;
+
+public class CheckPalindrom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input the number to be verified: ");
+        int number = scanner.nextInt();
+        int x, reverse = 0;
+        int a = number;
+
+        while (a!=0) {
+            x = a%10;
+            reverse = reverse*10 + x;
+            a = a/10;
+        }
+        if (reverse == number)
+            System.out.println("The number is a palindrome!");
+        else
+            System.out.println("The number is not a palindrome!");
+    }
+}
---
+++ src/main/java/Intro_to_java/Factorial.java
@@ -0,0 +1,20 @@
+package Intro_to_java;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input the number to calculate factorial: ");
+        int n = scanner.nextInt();
+
+        int a = 1;
+
+        for (int i = 1; i <= n; i++) {
+            a = a * i;
+        }
+        System.out.println("The factorial of " + n + " is: " + a);
+    }
+}
