fbe4065396134a557b040c5eee706cf28699d003
messer82 <podinamarius82@gmail.com>
2019-12-15 17:20:22 +0200

    Tema

--- src/main/java/OOP_homework/Punctul2/ContinuousCalculatorR.java
+++ src/main/java/OOP_homework/Punctul2/ContinuousCalculatorR.java
@@ -1,11 +1,8 @@
 package OOP_homework.Punctul2;
 
-import Intro_to_java.Calculator;
-
 public class ContinuousCalculatorR {
 
     public static void main(String[] args) {
-        CalculatorR.makeCalculus();
+        ContinuousCalculusR.makeCalculus();
     }
 }
-
