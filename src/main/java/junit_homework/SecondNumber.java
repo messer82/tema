5e09cf4a225437ce64e0eb00e7b92caed055a395
messer82 <podinamarius82@gmail.com>
2020-02-25 13:43:29 +0200

    JUnit homework - does not compute proper...

---
+++ src/main/java/junit_homework/SecondNumber.java
@@ -0,0 +1,67 @@
+package junit_homework;
+
+import java.util.Objects;
+
+public class SecondNumber {
+    private double distanceValue;
+    private String unitOfMeasurement;
+
+    public double getDistanceValue() {
+        return distanceValue;
+    }
+
+    public void setDistanceValue(double distanceValue) {
+        this.distanceValue = distanceValue;
+    }
+
+    public String getUnitOfMeasurement() {
+        return unitOfMeasurement;
+    }
+
+    public void setUnitOfMeasurement(String unitOfMeasurement) {
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+
+    @Override
+    public String toString() {
+        return "SecondNumber{" +
+                "distanceValue=" + distanceValue +
+                ", unitOfMeasurement='" + unitOfMeasurement + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SecondNumber that = (SecondNumber) o;
+        return Double.compare(that.distanceValue, distanceValue) == 0 &&
+                Objects.equals(unitOfMeasurement, that.unitOfMeasurement);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distanceValue, unitOfMeasurement);
+    }
+
+    public SecondNumber(double distanceValue, String unitOfMeasurement) {
+        this.distanceValue = distanceValue;
+        this.unitOfMeasurement = unitOfMeasurement;
+
+        if (unitOfMeasurement.equals("mm")) {
+            distanceValue = distanceValue * 1;
+        }
+        if (unitOfMeasurement.equals("cm")) {
+            distanceValue = distanceValue * 10;
+        }
+        if (unitOfMeasurement.equals("dm")) {
+            distanceValue = distanceValue * 100;
+        }
+        if (unitOfMeasurement.equals("m")) {
+            distanceValue = distanceValue * 1000;
+        }
+        if (unitOfMeasurement.equals("km")) {
+            distanceValue = distanceValue * 1000000;
+        }
+    }
+}
