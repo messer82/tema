2bfa584e3a38a674395bb5c11f47d1666a4027b1
messer82 <podinamarius82@gmail.com>
2020-03-16 13:55:54 +0200

    Java8 - punctul 1 - cred ca la asta te refereai.

--- src/main/java/java_8_homework/Fibonacci.java
+++ src/main/java/java_8_homework/Fibonacci.java
@@ -1,16 +1,22 @@
 package java_8_homework;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int[] fibonacciArray = new int[100];
-        fibonacciArray[0] = 0;
-        fibonacciArray[1] = 1;
-        for (int i = 2; i < 100; i++) {
-            fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
-        }
-        Arrays.stream(fibonacciArray).forEachOrdered(System.out::println);
+//        int[] fibonacciArray = new int[100];
+//        fibonacciArray[0] = 0;
+//        fibonacciArray[1] = 1;
+//        for (int i = 2; i < 100; i++) {
+//            fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
+//        }
+//        Arrays.stream(fibonacciArray).forEachOrdered(System.out::println);
+
+        Stream.iterate(new int[] {0, 1}, f -> new int[]{f[1], f[0] + f[1]})
+                .limit(100)
+                .map(f -> f[0])
+                .forEach(System.out::println);
     }
 }
